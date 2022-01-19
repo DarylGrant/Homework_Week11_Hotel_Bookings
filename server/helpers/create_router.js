cb611b4e44f5def0ce6381e46e05e3fab2c4f4dc
DarylGrant <darylkgrant@gmail.com>
2022-01-19 15:18:45 +0000

    Completed

---
+++ server/helpers/create_router.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function (collection) {
+
+  const router = express.Router();
+
+  router.get('/', (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err)
+        res.status(500)
+        res.json({ status: 500, error: err })
+      })
+  })
+
+  router.post('/', (req, res) => {
+    const newBooking = req.body
+    collection
+      .insertOne(newBooking)
+      .then((result) => {
+        res.json(result.ops[0])
+      })
+      .catch((err) => {
+        console.error(err)
+        res.status(500)
+        res.json({ status: 500, error: err })
+      })
+  })
+
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    collection
+      .deleteOne({ _id: ObjectID(id) })
+      .then(result => res.json(result))
+      .catch((err) => {
+        console.error(err)
+        res.status(500)
+        res.json({ status: 500, error: err })
+      })
+  })
+
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updateBooking = req.body
+    collection
+      .updateOne(
+        { _id: ObjectID(id) },
+        { $set: updateBooking }
+      )
+      .then((result) => res.json(result))
+      .catch((err) => {
+        console.error(err)
+        res.status(500)
+        res.json({ status: 500, error: err })
+      })
+    })
+
+    return router;
+  };
+
+  module.exports = createRouter;
